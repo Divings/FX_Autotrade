b7acdc3d7377cd5d05b4b60c4038f96dee0988f1
Divings <144668090+Divings@users.noreply.github.com>
2025-05-25 16:44:25 +0900

    Update conf_load.py

--- Source/Main/conf_load.py
+++ Source/Main/conf_load.py
@@ -13,7 +13,7 @@ def load_settings_from_db():
             'host': os.getenv('DB_HOST'),
             'port': int(os.getenv('DB_PORT', 3306)),
             'user': os.getenv('DB_USER'),
-            'password': os.getenv('DB_PASSWORD'),
+            'password': os.getenv('DB_PASS'),
             'database': os.getenv('DB_NAME')
         }
 
