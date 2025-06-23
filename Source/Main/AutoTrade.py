00f00ce4fc0d38c6b9e19692e6e56e7b6ed870c2
Divings <144668090+Divings@users.noreply.github.com>
2025-06-23 17:44:54 +0900

    SELL条件を更新

--- Source/Main/AutoTrade.py
+++ Source/Main/AutoTrade.py
@@ -1198,7 +1198,7 @@ async def monitor_trend(stop_event, short_period=6, long_period=13, interval_sec
 
             if trend == "BUY" and (macd_bullish or macd_cross_up) and sma_cross_up and rsi < 70 and adx >= 20 and rsi_limit and dmi_trend_match:
                 await process_entry(trend, shared_state, price_buffer,rsi_str,adx_str)
-            elif trend == "SELL" and (macd_bearish or macd_cross_down) and sma_cross_down and adx >= 20 and rsi > 30 and rsi_limit and dmi_trend_match:
+            elif trend == "SELL" and (macd_cross_down) and sma_cross_down and adx >= 20 and rsi > 35 and rsi_limit and dmi_trend_match and statistics.stdev(list(price_buffer)[-5:]) >= 0.007 and statistics.stdev(list(price_buffer)[-20:]) >= 0.010:
                 await process_entry(trend, shared_state, price_buffer, rsi_str,adx_str)
             elif positions and trend == "SELL" and (macd_bullish or macd_cross_up):
                 notify_slack(f"[トレンド] トレンド反転 即時損切り")
